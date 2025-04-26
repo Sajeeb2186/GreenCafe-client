8b36b6569743c04ab7da9c9d8f7f563d5ce8040b
Sajeeb <sajeeb2186@gmail.com>
2025-04-26 23:37:25 +0600

    navber and footer and banner complete

---
+++ src/Layout/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Footer from '../pages/shared/footer/Footer'
+import Navbar from '../pages/shared/navbar/Navbar'
+
+export default function Main() {
+  return (
+    <div>
+        <Navbar></Navbar>
+        <Outlet></Outlet>
+        <Footer></Footer>
+        
+    </div>
+  )
+}
---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "../banner/Banner";
+import Category from "../category/Category";
+
+
+export default function Home() {
+  return (
+    <div> 
+
+        <Banner></Banner>
+        <Category></Category>
+
+    </div>
+  )
+}
