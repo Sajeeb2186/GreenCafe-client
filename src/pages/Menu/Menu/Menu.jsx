a621964ed682dc75d492aeb0674b72d0dd73fe71
Sajeeb <sajeeb2186@gmail.com>
2025-05-02 23:41:15 +0600

    ssss

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,4 +1,4 @@
-import { Helmet } from 'react-helmet-async';
+
 import Cover from '../../shared/cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg';
 import MenuItems from '../../Home/menuItems/MenuItems';
@@ -6,9 +6,7 @@ import MenuItems from '../../Home/menuItems/MenuItems';
 export default function Menu() {
   return (
     <div>
-    <Helmet>
-        <title>GreenCafe | Menu</title>
-    </Helmet>
+   
 
     <Cover img={menuImg} title="Our menu"></Cover>
     <MenuItems></MenuItems><Cover img={menuImg} title="Our menu"></Cover>
